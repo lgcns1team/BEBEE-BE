38b11157b2e34702b862e40cc2108e77de48445a
khw7385 <khw7385@naver.com>
2025-12-02 18:55:59 +0900

    feature: 각 서비스의 common 모듈 빈 객체 탐색을 위한 탐색 범위 설정

--- chat-service/src/main/java/com/lgcns/bebee/chat/ChatServiceApplication.java
+++ chat-service/src/main/java/com/lgcns/bebee/chat/ChatServiceApplication.java
@@ -2,8 +2,10 @@ package com.lgcns.bebee.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.lgcns.bebee")
 public class ChatServiceApplication {
 
 	public static void main(String[] args) {
