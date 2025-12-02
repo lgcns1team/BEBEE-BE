bc869886688ac4380ef76e7cc2c4e0655020d1b6
khw7385 <khw7385@naver.com>
2025-12-02 18:52:07 +0900

    feature: 웹 설정 정의

---
+++ common/src/main/java/com/lgcns/bebee/common/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.lgcns.bebee.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+    }
+}
