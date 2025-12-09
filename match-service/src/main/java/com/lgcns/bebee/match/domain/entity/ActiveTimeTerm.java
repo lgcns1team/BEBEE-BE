a1d2cccaa314140dcfd5ad1cc0005a34b2681c5c
Hannaoo1 <hannaj5494@gmail.com>
2025-12-09 15:45:03 +0900

    feat: 지속 도움 활동 시간 엔티티 설정

---
+++ match-service/src/main/java/com/lgcns/bebee/match/domain/entity/ActiveTimeTerm.java
@@ -0,0 +1,33 @@
+package com.lgcns.bebee.match.domain.entity;
+
+import com.lgcns.bebee.common.domain.BaseTimeEntity;
+import io.hypersistence.utils.hibernate.id.Tsid;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+
+import java.util.Date;
+
+@Entity // 이거를 왜 사용하는지 고민하기
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ActiveTimeTerm extends BaseTimeEntity {
+
+    @Id @Tsid
+    @Column(name = "post_id", nullable = false)
+    private Long postId;
+
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    @JoinColumn(name = "post_id", nullable = false)
+    private HelpRequestPost post;
+
+    @Column(nullable = false)
+    private Date startDate;
+
+    @Column(nullable = false)
+    private Date endDate;
+
+}
