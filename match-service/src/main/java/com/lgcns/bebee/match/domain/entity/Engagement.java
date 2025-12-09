fb38d9b26999ec9ab9d84499e7ee4d6cb14ae3df
MinJaeSon <mjsonsj@gmail.com>
2025-12-09 15:04:48 +0900

    feat: 도움 활동 엔티티 설정

---
+++ match-service/src/main/java/com/lgcns/bebee/match/domain/entity/Engagement.java
@@ -0,0 +1,32 @@
+package com.lgcns.bebee.match.domain.entity;
+
+import com.lgcns.bebee.common.domain.BaseTimeEntity;
+import com.lgcns.bebee.match.domain.entity.vo.EngagementType;
+import io.hypersistence.utils.hibernate.id.Tsid;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Engagement extends BaseTimeEntity {
+    @Id
+    @Tsid
+    private Long engagementId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private Agreement agreement;
+
+    @Enumerated(EnumType.STRING)
+    private EngagementType type;
+
+    @Column
+    private Boolean isDisabledCheck = false;
+
+    @Column
+    private Boolean isHelperCheck = false;
+
+    @Column
+    private Long count = 0L;
+}
