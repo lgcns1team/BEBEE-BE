12eb8109401aea6c5f88c66e0c9b1c0330b8b244
MinJaeSon <mjsonsj@gmail.com>
2025-12-10 18:21:52 +0900

    chore: match 서비스 파라미터 에러 코드 관련 파일명 수정

--- match-service/src/main/java/com/lgcns/bebee/match/exception/MatchInvalidParamErrors.java
+++ match-service/src/main/java/com/lgcns/bebee/match/exception/MatchInvalidParamErrors.java
@@ -2,19 +2,17 @@ package com.lgcns.bebee.match.exception;
 
 import com.lgcns.bebee.common.exception.InvalidParamErrorInfo;
 
-public enum MatchInvalidParamException implements InvalidParamErrorInfo {
+public enum MatchInvalidParamErrors implements InvalidParamErrorInfo {
     /**
      * 파라미터 검증 에러 코드 정의
      * 아래는 예시로 작성. 개발 중 직접 추가 또는 수정 필요
      */
-    USER_NOT_FOUND("해당 유저를 찾을 수 없습니다.", "email"),
-    EMAIL_REQUIRED("이메일이 설정되지 않았습니다.", "email"),
-    EMAIL_ALREADY_EXISTS("이미 존재하는 이메일입니다.", "email");
+    MATCH_NOT_FOUND("매칭 정보를 찾을 수 없습니다.", "match");
 
     private final String desc;
     private final String field;
 
-    MatchInvalidParamException(String desc, String field) {
+    MatchInvalidParamErrors(String desc, String field) {
         this.desc = desc;
         this.field = field;
     }
