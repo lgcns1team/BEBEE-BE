88a27b70e38cf4f2a31f3666b7f21b5bf5d87d0d
khw7385 <khw7385@naver.com>
2025-12-11 16:12:41 +0900

    refactor: 웹소켓 엔드 포인트 이름 변경

--- chat-service/src/main/java/com/lgcns/bebee/chat/core/utils/StompDestinationUtils.java
+++ chat-service/src/main/java/com/lgcns/bebee/chat/core/utils/StompDestinationUtils.java
@@ -1,7 +1,7 @@
 package com.lgcns.bebee.chat.core.utils;
 
 public final class StompDestinationUtils {
-    public static final String ENDPOINT = "/ws/chat";
+    public static final String ENDPOINT = "/ws/chats";
     public static final String PUBLISH_PREFIX = "/pub";
     public static final String SUBSCRIBE_PREFIX = "/sub";
 
