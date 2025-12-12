71c8ea4ea305803258d52fb463aeeb31d346f2b3
khw7385 <khw7385@naver.com>
2025-12-12 19:15:43 +0900

    feat: Chat 서비스 에러 코드 및 예외 객체 정의

---
+++ chat-service/src/main/java/com/lgcns/bebee/chat/core/exception/ChatErrors.java
@@ -0,0 +1,24 @@
+package com.lgcns.bebee.chat.core.exception;
+
+import com.lgcns.bebee.common.exception.DomainException;
+import com.lgcns.bebee.common.exception.ErrorInfo;
+
+public enum ChatErrors implements ErrorInfo {
+    CHATROOM_NOT_FOUND("해당 채팅방을 찾을 수 없습니다.");
+
+    private final String desc;
+
+    ChatErrors(String desc){
+        this.desc = desc;
+    }
+
+    @Override
+    public String getDesc() {
+        return "";
+    }
+
+    @Override
+    public DomainException toException() {
+        return new ChatException(this);
+    }
+}
