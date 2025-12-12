f1dd976417ab5d558f4906e8c733f350e49becb4
MinJaeSon <mjsonsj@gmail.com>
2025-12-12 16:50:27 +0900

    feat: 스웨거 문서 추가

--- match-service/src/main/java/com/lgcns/bebee/match/presentation/AgreementController.java
+++ match-service/src/main/java/com/lgcns/bebee/match/presentation/AgreementController.java
@@ -3,6 +3,10 @@ package com.lgcns.bebee.match.presentation;
 import com.lgcns.bebee.match.application.usecase.CreateAgreementUseCase;
 import com.lgcns.bebee.match.presentation.dto.AgreementCreateReqDTO;
 import com.lgcns.bebee.match.presentation.dto.AgreementCreateResDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Agreement", description = "매칭 확인서 API")
 @RestController
 @RequestMapping("/api/agreements")
 @RequiredArgsConstructor
@@ -18,6 +23,12 @@ public class AgreementController {
 
     private final CreateAgreementUseCase createAgreementUseCase;
 
+    @Operation(summary = "매칭 확인서 생성", description = "새로운 매칭 확인서를 생성합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "매칭 확인서 생성 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+            @ApiResponse(responseCode = "404", description = "리소스를 찾을 수 없음")
+    })
     @PostMapping
     public ResponseEntity<AgreementCreateResDTO> createAgreement(@RequestBody AgreementCreateReqDTO request) {
 
