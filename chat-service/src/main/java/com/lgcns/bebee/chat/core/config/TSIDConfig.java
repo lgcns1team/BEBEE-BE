604df6d69cc4863b0b4392eb6b25555d11ea8a4c
khw7385 <khw7385@naver.com>
2025-12-13 13:04:26 +0900

    feat: TSID 설정

---
+++ chat-service/src/main/java/com/lgcns/bebee/chat/core/config/TSIDConfig.java
@@ -0,0 +1,8 @@
+package com.lgcns.bebee.chat.core.config;
+
+import com.lgcns.bebee.common.config.BaseTSIDConfig;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TSIDConfig extends BaseTSIDConfig {
+}
---
+++ common/src/main/java/com/lgcns/bebee/common/config/BaseTSIDConfig.java
@@ -0,0 +1,36 @@
+package com.lgcns.bebee.common.config;
+
+import io.hypersistence.tsid.TSID;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+@Slf4j
+public abstract class BaseTSIDConfig {
+
+    /**
+     * TSID Factory 빈 등록
+     * 각 서비스에서 상속하여 사용
+     *
+     * @param nodeId 노드 식별자 (환경 변수에서 주입)
+     * @return TSID Factory 인스턴스
+     */
+    @Bean
+    public TSID.Factory tsidFactory(@Value("${tsid.node.id}") int nodeId){
+        int nodeBits = 10;
+        int maxNodeId = (1 << nodeBits) - 1;
+
+        if (nodeId < 0 || nodeId > maxNodeId){
+            throw new IllegalArgumentException(
+                String.format("Node ID must be between 0 and %d, but got %d", maxNodeId, nodeId)
+            );
+        }
+
+        TSID.Factory factory = TSID.Factory.builder()
+                .withNode(nodeId)
+                .withNodeBits(nodeBits)
+                .build();
+
+        return factory;
+    }
+}
