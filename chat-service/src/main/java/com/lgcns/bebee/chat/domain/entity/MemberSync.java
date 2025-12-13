871fe19dd1723d94e2fd52eecbf00b27531de5fa
khw7385 <50816902+khw7385@users.noreply.github.com>
2025-12-13 21:09:35 +0900

    feature/BB-56: 채팅 서비스 SYNC 테이블 엔티티 작성 (#24)
    * docs: 채팅 서비스 싱크 테이블에 대한 flyway 문서 작성
    
    * feat: Member Sync 엔티티 구현

---
+++ chat-service/src/main/java/com/lgcns/bebee/chat/domain/entity/MemberSync.java
@@ -0,0 +1,27 @@
+package com.lgcns.bebee.chat.domain.entity;
+
+import com.lgcns.bebee.common.domain.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberSync extends BaseTimeEntity {
+
+    @Id
+    @Column(name = "member_id")
+    private Long id;
+
+    private String nickname;
+
+    private String profileImageUrl;
+
+    private BigDecimal sweetness;
+}
