6db8696726fc6a98de0d5742391992a9bb5cec4b
YongjaeKwon0629 <dydrkfl0218@naver.com>
2025-12-14 16:56:17 +0900

    feat: Redis 설정 클래스 추가

---
+++ member-service/src/main/java/com/lgcns/bebee/member/infrastructure/config/RedisConfig.java
@@ -0,0 +1,28 @@
+package com.lgcns.bebee.member.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * Redis 설정
+ * 리프레시 토큰 블랙리스트 관리를 위한 RedisTemplate 설정
+ */
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new StringRedisSerializer());
+        template.afterPropertiesSet();
+        return template;
+    }
+}
+
