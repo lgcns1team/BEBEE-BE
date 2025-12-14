b674d2f8258d61f45330af1271c658c9986ef712
khw7385 <khw7385@naver.com>
2025-12-14 21:19:09 +0900

    feat: 채팅방 리스트 조회 로직 구현

--- chat-service/src/main/java/com/lgcns/bebee/chat/presentation/ChatroomController.java
+++ chat-service/src/main/java/com/lgcns/bebee/chat/presentation/ChatroomController.java
@@ -2,8 +2,10 @@ package com.lgcns.bebee.chat.presentation;
 
 import com.lgcns.bebee.chat.application.GetChatMessagesUseCase;
 import com.lgcns.bebee.chat.application.GetChatroomUseCase;
+import com.lgcns.bebee.chat.application.GetChatroomsUseCase;
 import com.lgcns.bebee.chat.presentation.dto.res.ChatMessagesGetResDTO;
 import com.lgcns.bebee.chat.presentation.dto.res.ChatroomGetResDTO;
+import com.lgcns.bebee.chat.presentation.dto.res.ChatroomsGetResDTO;
 import com.lgcns.bebee.chat.presentation.swagger.ChatroomSwagger;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 public class ChatroomController implements ChatroomSwagger {
     private final GetChatMessagesUseCase getChatMessagesUseCase;
     private final GetChatroomUseCase getChatroomUseCase;
+    private final GetChatroomsUseCase getChatroomsUseCase;
 
     @GetMapping("/chats")
     public ResponseEntity<ChatMessagesGetResDTO> getChatMessages(
@@ -30,6 +33,20 @@ public class ChatroomController implements ChatroomSwagger {
                 result.isHasNext(),
                 result.getNextChatId()
         );
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<ChatroomsGetResDTO> getChatrooms(
+            @RequestParam Long currentMemberId,
+            @RequestParam(required = false) Long lastChatroomId,
+            @RequestParam(defaultValue = "20") Integer count
+    ) {
+        GetChatroomsUseCase.Param param = new GetChatroomsUseCase.Param(currentMemberId, lastChatroomId, count);
+        GetChatroomsUseCase.Result result = getChatroomsUseCase.execute(param);
+
+        ChatroomsGetResDTO response = ChatroomsGetResDTO.from(result, currentMemberId);
 
         return ResponseEntity.ok(response);
     }
