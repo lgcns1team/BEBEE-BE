6e7d7a66a1a2c6fbd20685a6be62a350515da8c0
khw7385 <khw7385@naver.com>
2025-12-17 22:22:47 +0900

    feat: PushNotificationSubscription 도메인 객체 생성 로직 구현

--- notification-service/src/main/java/com/lgcns/bebee/notification/domain/entity/PushNotificationSubscription.java
+++ notification-service/src/main/java/com/lgcns/bebee/notification/domain/entity/PushNotificationSubscription.java
@@ -1,23 +1,40 @@
 package com.lgcns.bebee.notification.domain.entity;
 
 import com.lgcns.bebee.common.domain.BaseTimeEntity;
+import com.lgcns.bebee.notification.domain.entity.vo.DeviceType;
 import io.hypersistence.utils.hibernate.id.Tsid;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PushNotificationSubscription extends BaseTimeEntity {
     @Id
     @Tsid
-    private Long webPushNotificationId;
+    private Long id;
 
     @Column(nullable = false)
     private Long subscriberId;
 
-    @Column(nullable = false)
-    private Long fcmTokenId;
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Enumerated(EnumType.STRING)
+    private DeviceType deviceType;
+
+    public static PushNotificationSubscription create(
+            Long subscriberId,
+            String token,
+            String deviceType
+    ){
+        PushNotificationSubscription subscription = new PushNotificationSubscription();
+        subscription.subscriberId = subscriberId;
+        subscription.token = token;
+        subscription.deviceType = DeviceType.from(deviceType);
+
+        return subscription;
+    }
 }
