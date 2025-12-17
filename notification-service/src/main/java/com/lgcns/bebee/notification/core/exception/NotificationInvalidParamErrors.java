4669ecd7b480ac6888ad33fe4d6ef7120fc496c8
khw7385 <khw7385@naver.com>
2025-12-17 22:23:23 +0900

    feat: 알림 서비스 예외 처리

---
+++ notification-service/src/main/java/com/lgcns/bebee/notification/core/exception/NotificationInvalidParamErrors.java
@@ -0,0 +1,25 @@
+package com.lgcns.bebee.notification.core.exception;
+
+import com.lgcns.bebee.common.exception.InvalidParamErrorInfo;
+
+public enum NotificationInvalidParamErrors implements InvalidParamErrorInfo {
+    INVALID_DEVICE_TYPE("유효하지 않은 디바이스 타입입니다.", "deviceType");
+
+    private final String desc;
+    private final String field;
+
+    NotificationInvalidParamErrors(String desc, String field){
+        this.desc = desc;
+        this.field = field;
+    }
+
+    @Override
+    public String getDesc() {
+        return "";
+    }
+
+    @Override
+    public String getField() {
+        return "";
+    }
+}
