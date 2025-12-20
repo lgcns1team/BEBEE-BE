58158b16666cdcf7347d5f7c4e062b106e839834
khw7385 <khw7385@naver.com>
2025-12-20 13:53:44 +0900

    feat: 회원 id로 알림 구독 정보를 찾지 못하는 에러 코드 추가

--- notification-service/src/main/java/com/lgcns/bebee/notification/core/exception/NotificationErrors.java
+++ notification-service/src/main/java/com/lgcns/bebee/notification/core/exception/NotificationErrors.java
@@ -4,7 +4,8 @@ import com.lgcns.bebee.common.exception.DomainException;
 import com.lgcns.bebee.common.exception.ErrorInfo;
 
 public enum NotificationErrors implements ErrorInfo {
-    TOKEN_ALREADY_EXISTS("이미 등록된 FCM 토큰입니다.");
+    TOKEN_ALREADY_EXISTS("이미 등록된 FCM 토큰입니다."),
+    SUBSCRIPTION_NOT_FOUND("해당 회원에 대한 구독 정보를 찾을 수 없습니다.");
 
     private final String desc;
 
