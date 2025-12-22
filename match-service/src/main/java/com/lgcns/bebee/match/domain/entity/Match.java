3db6284f235d5600a9801d6079b353f18df5a2a3
MinJaeSon <mjsonsj@gmail.com>
2025-12-22 16:48:52 +0900

    fix: sql 예약어 이스케이프 설정

--- match-service/src/main/java/com/lgcns/bebee/match/domain/entity/Match.java
+++ match-service/src/main/java/com/lgcns/bebee/match/domain/entity/Match.java
@@ -7,6 +7,7 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "`match`")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Match extends BaseTimeEntity {
     @Id
