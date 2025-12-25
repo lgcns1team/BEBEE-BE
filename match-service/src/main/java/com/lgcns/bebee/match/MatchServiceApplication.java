a4eaf9b6b2cb9f5b073293f07587bc6a4c2bd130
khw7385 <khw7385@naver.com>
2025-12-25 18:53:33 +0900

    feature: 프로퍼티 스캔 설정

--- match-service/src/main/java/com/lgcns/bebee/match/MatchServiceApplication.java
+++ match-service/src/main/java/com/lgcns/bebee/match/MatchServiceApplication.java
@@ -2,10 +2,12 @@ package com.lgcns.bebee.match;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.lgcns.bebee")
+@ConfigurationPropertiesScan(basePackages = "com.lgcns.bebee")
 public class MatchServiceApplication {
 
 	public static void main(String[] args) {
