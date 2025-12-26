0701ce502c8a54ab0673754ac8a2b5ffd39a67c7
MinJaeSon <mjsonsj@gmail.com>
2025-12-26 18:22:29 +0900

    refactor: 매칭 확인서 날짜(기간), 요일 및 시간 엔티티 구조 변경

--- match-service/src/main/resources/db/migration/V1__Init_Match_DB_Table.sql
+++ match-service/src/main/resources/db/migration/V1__Init_Match_DB_Table.sql
@@ -14,6 +14,7 @@ DROP TABLE IF EXISTS `post_schedule`;
 DROP TABLE IF EXISTS `post_period`;
 DROP TABLE IF EXISTS `post`;
 DROP TABLE IF EXISTS `agreement`;
+DROP TABLE IF EXISTS `match_member_sync`;
 
 -- CREATE TABLES
 
